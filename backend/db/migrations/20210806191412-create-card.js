35ce268f200d823337c3411919291b6a2496b82d
kevjinx <kevin415mei@gmail.com>
2021-08-07 10:36:57 -0700

    update card migration

--- backend/db/migrations/20210806191412-create-card.js
+++ backend/db/migrations/20210806191412-create-card.js
@@ -25,27 +25,21 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       factionId: {
-        references: { model: 'Factions' },
         type: Sequelize.INTEGER
       },
       typeId: {
-        references: { model: 'Types' },
         type: Sequelize.INTEGER
       },
       categoryId: {
-        references: { model: 'Categories' },
         type: Sequelize.ARRAY(Sequelize.INTEGER)
       },
       keywordId: {
-        references: { model: 'Keywords' },
         type: Sequelize.ARRAY(Sequelize.INTEGER)
       },
       rarityId: {
-        references: { model: 'Rarities' },
         type: Sequelize.INTEGER
       },
       setId: {
-        references: { model: 'Sets' },
         type: Sequelize.INTEGER
       },
       createdAt: {
