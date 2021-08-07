b77d52a2199819f51b4770ef10ca6f376d9567f8
kevjinx <kevin415mei@gmail.com>
2021-08-07 16:17:19 -0700

    commenting out a testseeder file to avoid build test errors

--- backend/db/seeders/20210807173410-cardSeedtest1.js
+++ backend/db/seeders/20210807173410-cardSeedtest1.js
@@ -1,26 +1,26 @@
-'use strict';
+// 'use strict';
 
-module.exports = {
-  up: (queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
+// module.exports = {
+//   up: (queryInterface, Sequelize) => {
+//     /*
+//       Add altering commands here.
+//       Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkInsert('People', [{
-        name: 'John Doe',
-        isBetaMember: false
-      }], {});
-    */
-  },
+//       Example:
+//       return queryInterface.bulkInsert('People', [{
+//         name: 'John Doe',
+//         isBetaMember: false
+//       }], {});
+//     */
+//   },
 
-  down: (queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
+//   down: (queryInterface, Sequelize) => {
+//     /*
+//       Add reverting commands here.
+//       Return a promise to correctly handle asynchronicity.
 
-      Example:
-      return queryInterface.bulkDelete('People', null, {});
-    */
-  }
-};
+//       Example:
+//       return queryInterface.bulkDelete('People', null, {});
+//     */
+//   }
+// };
