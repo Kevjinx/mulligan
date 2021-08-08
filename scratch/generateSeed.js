d574bfa23b2283cb4a2fc84302f1f0be5e1d086e
kevjinx <kevin415mei@gmail.com>
2021-08-07 18:26:56 -0700

    add back categories and keywords into simplied card json

--- scratch/generateSeed.js
+++ scratch/generateSeed.js
@@ -1,2 +1,31 @@
 //take from card keys and create seed
-const cardKeys = require('../backend/db/card-api/card-keys')
+const {keywordArr, categoryArr} = require('../backend/db/card-api/card-keys')
+const cardJson = require('../backend/db/card-api/cards-simplified.json')
+const fs = require('fs')
+
+//seeders to generate:
+// keywords
+// categories
+// cards
+
+const keywordSeeder = []
+const categorySeeder = []
+const cardSeeder = []
+
+const script = () => {
+
+	keywordArr.forEach(keyword => {
+		const seed = { name: keyword}
+		keywordSeeder.push(seed)
+	})
+
+	categoryArr.forEach(cat => {
+		const seed = { name: cat }
+		categorySeeder.push(seed)
+	})
+
+
+
+}
+
+script()
